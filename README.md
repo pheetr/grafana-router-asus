dea8ad07a3bb0044d5767e2793370b634e5fe636
pheetr <44616229+pheetr@users.noreply.github.com>
2021-04-19 16:32:00 +0100

    wireless client stats added
    Extended capabilities to cover wireless client metrics via a shell script that can be run on schedule to push relevant data into InfluxDB.

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # ASUS router metrics in Grafana (via SNMP)
 Config files and scripts for pulling ASUS router metrics into TIG stack via SNMP.
 
-![Grafana Dashboard](./grafana/grafana-asus.png)
+![Grafana Dashboard - Router](./grafana/grafana-asus.png)
 
-The implementation takes advantage of [SNMP Extend](http://net-snmp.sourceforge.net/wiki/index.php/Tut:Extending_snmpd_using_shell_scripts) to pull additional metrics (not covered by MIBs) through custom commands.
+![Grafana Dashboard - Router - Wireless Clients](./grafana/grafana-asus-wireless_clients.png)
+
+The implementation takes advantage of [SNMP Extend](http://net-snmp.sourceforge.net/wiki/index.php/Tut:Extending_snmpd_using_shell_scripts) to pull additional metrics (not covered by MIBs) through custom commands, as well as a shell script to pull wireless client metrics (second image) and push the data directly into InfluxDB.
 
 Tested with ASUS RT-AX86U running Asuswrt-Merlin v386.1_2.
 ## Pre-requisites
 1. [Asuswrt-Merlin](https://www.asuswrt-merlin.net/) firmware installed on the router.
 2. SNMP enabled on router. (Menu: Administration -> SNMP)
 3. JFFS custom scripts and configs enabled on router. (Menu: Administration -> System -> Persistent JFFS2 partition -> Enable JFFS custom scripts and configs)
-4. TIG stack deployed.
+4. Awareness of how to configure [user scripts](https://github.com/RMerl/asuswrt-merlin.ng/wiki/User-scripts) and [scheduled tasks](https://github.com/RMerl/asuswrt-merlin.ng/wiki/Scheduled-tasks-(cron-jobs)) on router.  
+5. TIG stack deployed.
 ## Contents
-1. Router [SNMP config append file](./asus-config/snmpd.conf.add) to define SNMP Extend
-2. [Shell scripts](./asus-scripts/) for pulling the following metrics on the router:
+1. Router [SNMP config append file](./asus-snmp_config/snmpd.conf.add) to define SNMP Extend
+2. SNMP Extend [shell scripts](./asus-snmp_scripts/) for pulling the following metrics on the router:
 	* Connection count
 	* Active connection count
 	* Admin web session - source IP of active/last session
@@ -27,8 +30,10 @@ Tested with ASUS RT-AX86U running Asuswrt-Merlin v386.1_2.
 	* Temperature - 5 GHz Wireless Module
 	* Wireless client count - 2.4 GHz
 	* Wireless client count - 5 GHz
-3. Grafana [dashboard config file](./grafana/router-asus.json) (uses [Flux](https://docs.influxdata.com/influxdb/v2.0/query-data/get-started/) to query InfluxDB)
+3. [Shell script](./asus-scripts/) for pulling wireless client metrics and pushing the data directly into InfluxDB (in-memory processing on router) 
+3. Grafana [dashboard config files](./grafana/) (use [Flux](https://docs.influxdata.com/influxdb/v2.0/query-data/get-started/) to query InfluxDB)
 4. Telegraf [router SNMP config](./telegraf/snmp_asus.conf)
 ## Configuration notes
 * Individual (per-metric) scripts are being used for SNMP Extend so as to avoid commands being run unnecessarily when the OIDs are being queried.
 * The Telegraf config file I've based my config on is sourced from [bretmlw](https://github.com/bretmlw/rt-n66u-snmp-telegraf-grafana). I've commented out everything that didn't return results for me, as well as things that I'm not planning on using near-term. I've also added comments to a number of OIDs from their respective MIBs for (hopefully) improved clarity.
+* The shell script for gathering wireless client metrics is based on [mod_wifi_clients.sh](https://github.com/corgan2222/extstats/blob/master/mod_wifi_clients.sh) from corgan2222's excellent [extstats](https://github.com/corgan2222/extstats) add-on.
---
+++ asus-scripts/wifi_client_stats.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+[ -z "$(nvram get odmpid)" ] && ROUTER_MODEL=$(nvram get productid) || ROUTER_MODEL=$(nvram get odmpid)   # "nvram get odmpid" doesn't return value on RT-AX86U 
+readonly SCRIPT_NAME="wifi_client_stats"
+readonly DATA_TEMP_FILE=""
+readonly DB_URL=""
+readonly DB_ORG=""
+readonly DB_BUCKET=""
+readonly DB_TOKEN=""
+
+
+wifi_client_stats()
+{
+
+  wifiInterface=$(nvram get wl_ifnames)
+  #echo $wifiInterface   #debug
+
+  for wlan in $wifiInterface; do
+    clients=$(wl -i $wlan assoclist | awk '{print $2}')   #getting wireless client MAC addresses
+    #printf "$wlan \n"   #debug
+
+        for client in $clients; do
+            #ip=$(grep -i $client /proc/net/arp | awk '{print $1}') #arp provided two IPs for a MAC address, even though VM on that system had unique MAC setting -> alternative source preferred
+			ip=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $3}') #grabbing IP from current DHCP lease table
+    	    host=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $4}') #grabbing hostname from current DHCP lease table
+			
+            if [ -z "$ip" ]; then
+                ip=$clients
+            fi
+
+            if [ -z "$host" ]; then
+                host=$clients
+            fi
+
+            #printf "$wlan \t "$client" \t "$ip" \t "$host" \\n"   #debug
+
+            #wl -i $wlan sta_info $client   #debug all wifi infos
+			
+			#unit for rssi and noise metrics: dB
+            rssi_antenna1=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $9}')
+            rssi_antenna2=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $10}')
+            rssi_antenna3=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $11}')
+            rssi_antenna4=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $12}')
+
+            rssi_antenna1_avg=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $9}')
+            rssi_antenna2_avg=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $10}')
+            rssi_antenna3_avg=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $11}')
+            rssi_antenna4_avg=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $12}')
+
+            noise_antenna1=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $5}')
+            noise_antenna2=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $6}')
+            noise_antenna3=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $7}')
+            noise_antenna4=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $8}')
+
+            #unit for rx and tx rate metrics: kbps
+			rx_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last rx pkt:/ {print $6}')
+            tx1_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $6}')
+            tx2_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $9}')
+			
+			#total data transferred, in bytes
+			rx_total_bytes=$(wl -i $wlan sta_info $client | awk '/rx data bytes/ {print $4}')
+			tx_total_bytes=$(wl -i $wlan sta_info $client | awk '/tx total bytes/ {print $4}')
+			
+            #link bandwidth unit: MHz
+			link_bandwidth=$(wl -i $wlan sta_info $client | awk '/link bandwidth/ {print $4}')
+            tx_failures=$(wl -i $wlan sta_info $client | awk '/tx failures:/ {print $3}')
+            rx_decrypt_failures=$(wl -i $wlan sta_info $client | awk '/rx decrypt failures:/ {print $4}')
+
+            #idle and online unit: seconds
+			idle=$(wl -i $wlan sta_info $client | awk '/idle/ {print $2}')
+            online=$(wl -i $wlan sta_info $client | awk '/in network/ {print $3}')
+            #rssi unit: dB
+			rssi=$(wl -i $wlan rssi $client)
+
+            if [ "$wlan" = eth6 ]; then 
+			chann="2.4GHz"; else 
+			chann="5GHz"; 
+			fi
+
+            tags="host=${ROUTER_MODEL},wifi=$wlan,client=$client,ip=$ip,hostname=$host,wifiBand=$chann"
+            fields="tx_total_bytes=$tx_total_bytes,rx_total_bytes=$rx_total_bytes,tx2_rate_pkt=$tx2_rate_pkt,tx1_rate_pkt=$tx1_rate_pkt,rx_rate_pkt=$rx_rate_pkt,rssi=$rssi,online=$online,idle=$idle,rssi_antenna1=$rssi_antenna1,rssi_antenna2=$rssi_antenna2,rssi_antenna3=$rssi_antenna3,rssi_antenna4=$rssi_antenna4,rssi_antenna1_avg=$rssi_antenna1_avg,rssi_antenna2_avg=$rssi_antenna2_avg,rssi_antenna3_avg=$rssi_antenna3_avg,rssi_antenna4_avg=$rssi_antenna4_avg,noise_antenna1=$noise_antenna1,noise_antenna2=$noise_antenna2,noise_antenna3=$noise_antenna3,noise_antenna4=$noise_antenna4,link_bandwidth=$link_bandwidth,tx_failures=$tx_failures,rx_decrypt_failures=$rx_decrypt_failures"
+            #echo $fields   #debug
+
+            curdate=`date +%s` #date in seconds
+			#curdate_="${curdate}000000000" #in case date needed in ns (InfluxDB 1.x)
+            measurement="router_asus_wificlients"
+            data="$measurement,$tags $fields $curdate"
+
+            #echo $data >> $DATA_TEMP_FILE   #debug
+            #echo $data   #debug
+            #curl -is -XPOST "https://${DB_URL}/write?db=${DB_NAME}&u=${USER}&p=${PASS}" --data-binary "$data"  > /dev/null   #InfluxDB 1.x
+            curl --insecure --request POST "https://${DB_URL}:8086/api/v2/write?org=${DB_ORG}&bucket=${DB_BUCKET}&precision=s" --header "Authorization: Token ${DB_TOKEN}" --data-raw "$data" #> /dev/null
+        done;
+  done;
+}
+
+#rm -f $DATA_TEMP_FILE
+wifi_client_stats
