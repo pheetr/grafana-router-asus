668f937b181b59d6eb9c90c90a5dcf8eec6910d5
pheetr <44616229+pheetr@users.noreply.github.com>
2021-04-19 16:40:42 +0100

    tweak

--- asus-scripts/wifi_client_stats.sh
+++ asus-scripts/wifi_client_stats.sh
@@ -21,9 +21,9 @@ wifi_client_stats()
 
         for client in $clients; do
             #ip=$(grep -i $client /proc/net/arp | awk '{print $1}') #arp provided two IPs for a MAC address, even though VM on that system had unique MAC setting -> alternative source preferred
-			ip=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $3}') #grabbing IP from current DHCP lease table
-    	    host=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $4}') #grabbing hostname from current DHCP lease table
-			
+            ip=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $3}') #grabbing IP from current DHCP lease table
+            host=$(grep -i $client /var/lib/misc/dnsmasq.leases | awk '{print $4}') #grabbing hostname from current DHCP lease table
+
             if [ -z "$ip" ]; then
                 ip=$clients
             fi
@@ -53,36 +53,36 @@ wifi_client_stats()
             noise_antenna4=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $8}')
 
             #unit for rx and tx rate metrics: kbps
-			rx_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last rx pkt:/ {print $6}')
+            rx_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last rx pkt:/ {print $6}')
             tx1_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $6}')
             tx2_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $9}')
-			
-			#total data transferred, in bytes
-			rx_total_bytes=$(wl -i $wlan sta_info $client | awk '/rx data bytes/ {print $4}')
-			tx_total_bytes=$(wl -i $wlan sta_info $client | awk '/tx total bytes/ {print $4}')
-			
+
+            #total data transferred, in bytes
+            rx_total_bytes=$(wl -i $wlan sta_info $client | awk '/rx data bytes/ {print $4}')
+            tx_total_bytes=$(wl -i $wlan sta_info $client | awk '/tx total bytes/ {print $4}')
+
             #link bandwidth unit: MHz
-			link_bandwidth=$(wl -i $wlan sta_info $client | awk '/link bandwidth/ {print $4}')
+            link_bandwidth=$(wl -i $wlan sta_info $client | awk '/link bandwidth/ {print $4}')
             tx_failures=$(wl -i $wlan sta_info $client | awk '/tx failures:/ {print $3}')
             rx_decrypt_failures=$(wl -i $wlan sta_info $client | awk '/rx decrypt failures:/ {print $4}')
 
             #idle and online unit: seconds
-			idle=$(wl -i $wlan sta_info $client | awk '/idle/ {print $2}')
+            idle=$(wl -i $wlan sta_info $client | awk '/idle/ {print $2}')
             online=$(wl -i $wlan sta_info $client | awk '/in network/ {print $3}')
             #rssi unit: dB
-			rssi=$(wl -i $wlan rssi $client)
+            rssi=$(wl -i $wlan rssi $client)
 
             if [ "$wlan" = eth6 ]; then 
-			chann="2.4GHz"; else 
-			chann="5GHz"; 
-			fi
+              chann="2.4GHz"; else 
+              chann="5GHz"; 
+            fi
 
             tags="host=${ROUTER_MODEL},wifi=$wlan,client=$client,ip=$ip,hostname=$host,wifiBand=$chann"
             fields="tx_total_bytes=$tx_total_bytes,rx_total_bytes=$rx_total_bytes,tx2_rate_pkt=$tx2_rate_pkt,tx1_rate_pkt=$tx1_rate_pkt,rx_rate_pkt=$rx_rate_pkt,rssi=$rssi,online=$online,idle=$idle,rssi_antenna1=$rssi_antenna1,rssi_antenna2=$rssi_antenna2,rssi_antenna3=$rssi_antenna3,rssi_antenna4=$rssi_antenna4,rssi_antenna1_avg=$rssi_antenna1_avg,rssi_antenna2_avg=$rssi_antenna2_avg,rssi_antenna3_avg=$rssi_antenna3_avg,rssi_antenna4_avg=$rssi_antenna4_avg,noise_antenna1=$noise_antenna1,noise_antenna2=$noise_antenna2,noise_antenna3=$noise_antenna3,noise_antenna4=$noise_antenna4,link_bandwidth=$link_bandwidth,tx_failures=$tx_failures,rx_decrypt_failures=$rx_decrypt_failures"
             #echo $fields   #debug
 
             curdate=`date +%s` #date in seconds
-			#curdate_="${curdate}000000000" #in case date needed in ns (InfluxDB 1.x)
+            #curdate_="${curdate}000000000" #in case date needed in ns (InfluxDB 1.x)
             measurement="router_asus_wificlients"
             data="$measurement,$tags $fields $curdate"
 
